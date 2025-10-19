a5203d7e134edb9f13908f03b4bc254b28a2f858
Arthur511 <92777780+Arthur511@users.noreply.github.com>
2025-10-19 16:59:32 +0200

    [GP] Fix some details 3

--- Assets/Scripts/Gameplay/MainGameplay.cs
+++ Assets/Scripts/Gameplay/MainGameplay.cs
@@ -228,7 +228,7 @@ public class MainGameplay : MonoBehaviour
             go.SetActive(false);
         }
         yield return new WaitForSeconds(10f);
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene(0);
     }
 
 
